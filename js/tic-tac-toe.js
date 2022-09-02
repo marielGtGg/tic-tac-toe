fa7fde2ce7745fe3c75dc085a12a7adc651e34df
chriroux <112137056+chriroux@users.noreply.github.com>
2022-09-01 22:08:48 -0400

    Environnement christina

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -11,6 +11,10 @@ class gameTicTacToe {
         this.name = "";
         this.board = document.querySelector("#pluginGameBoard");
         this.cells = document.getElementsByClassName("pluginGameCell");
+        this.scores = {
+            X: 0,
+            O: 0,
+        };
         this.scoreHost = [0, 0, 0]; /* Win/Lost/Tie */
         this.scoreGuest = [0, 0, 0]; /* Win/Lost/Tie */
         this.players = ["x", "o"];
@@ -28,74 +32,114 @@ class gameTicTacToe {
         ];
         this.setActivePlayer() 
     }
+/*
+function updateScores(X, O) {
+	document.querySelector("#pluginGuestPlayer #pluginGuestWon").innerHTML = X;
+	document.querySelector("#pluginGuestPlayer #pluginHostWon").innerHTML = O;	
+}
+    updateScore() {
+        console.log(`Host ${this.scoreHost[0]} win`);
+        console.log(`Host ${this.scoreHost[1]} lost`);
+        console.log(`Host ${this.scoreHost[2]} tie`);
+        console.log(`Guest ${this.scoreHost[1]} win`);
+        console.log(`Guest ${this.scoreHost[2]} lost`);
+        console.log(`Guest ${this.scoreHost[0]} tie`);
+    }
+*/
+/*winVerif() {
+        for (let i = 0; i < this.winCondition.length; i++) {
+            if (this.winCondition[i][0] === this.winCondition[i][1] === this.winCondition[i][2]) {
+                this.board.classList.add("");
+            } else {
+                this.board.classList.add("null");
+            }
+        }
+    }
+*/
+/*var placeholder = document.getElementById("placeholder");
 
+var tictactoe = new TicTacToe(placeholder, 1, onResult);
+
+function onResult(result, scores) {
+	if(result == 'draw') {
+		alert("It's a draw !");
+	} else {
+		alert(result + " has won");
+		updateScores(scores.X, scores.O);
+	}
+	tictactoe.empty();
+}
+
+
+    
+    }
+*/
     /*Fonction à lier au bouton reset*/
     resetBoard() {;
-        for (var i=0; i < this.cells.length; i++){
+        for (var i=0; i< this.cells.length; i++){
             this.cells[i].classList.remove("o", "x", "empty");
             this.cells[i].classList.add("empty");
         }; 
-        this.setEventToGrid();  //retire et recrée les eventlistner   
+        this.setEventToGrid(this);  //retire et recrée les eventlistner   
         this.setActivePlayer(); //redétermine le premier joueur de façon aléatoire
     }
 
     /*Fonction à placer les event sur tout les cases de la grid*/
-    setEventToGrid() {
+    setEventToGrid(game) {
         //j'ai mis la fonction dans une constante parce que
         //si on la passe en fonction anonyme direment dans le evetListener, 
         //le removeEventListener ne reconnait pas que c'est la même fonction et ne fonctionne pas.
-        let self = this //permet de référer à la classe dans la fonction imbriquée
         const onClick = function() {
-            playTurn(this); //Ici this réfère à l'élément cliqué (la cellule)
-            //TODO: fonction qui vérifie les conditions de victoire 
+            playTurn(this, game);
         }
 
-        for (var i=0; i < this.cells.length; i++){
-            this.cells[i].replaceWith(this.cells[i].cloneNode()); //retire tous les eventListeners (pour le cas d'un reset où il reste des eventListener sur certaines cases)
-            this.cells[i].addEventListener('click', onClick);
+        for (var i=0; i < game.cells.length; i++){
+            game.cells[i].replaceWith(game.cells[i].cloneNode()); //retire tous les eventListeners (pour le cas d'un reset où il reste des eventListener sur certaines cases)
+            game.cells[i].addEventListener('click', onClick);
         }
 
-        function playTurn(clickedCell) {
-            clickedCell.classList.remove("empty");
-            clickedCell.classList.add(self.activePlayer);
-            clickedCell.removeEventListener('click', onClick); //empêche de rejouer sur la même case
-            self.switchPlayer();
-        }   
-    }
+        function playTurn(cell, game) {
+            cell.classList.remove("empty");
+            cell.classList.add(game.activePlayer);
+            cell.removeEventListener('click', onClick); //empêche de rejouer sur la même case
+            game.switchPlayer(game);
+        }
+
 
     setActivePlayer() {
         //pour le moment déternime le premier joueur de façon aléatoire
         this.activePlayer = this.players[Math.floor(Math.random() * 2)];
-        this.switchPlayer();
+        this.switchPlayer(this);
     }
     
-    switchPlayer() {
-        this.activePlayer = this.activePlayer == this.players[0] ? this.players[1] : this.players[0];
+    switchPlayer(game) {
+        game.activePlayer = game.activePlayer == game.players[0] ? game.players[1] : game.players[0];
         
         //Pour le hover sur les cases de la grille
-        this.board.classList.remove("x", "o"); 
-        this.board.classList.add(this.activePlayer);
+        game.board.classList.remove("x", "o"); 
+        game.board.classList.add(this.activePlayer);
 
         //Pour la section des joueurs
-        this.host.classList.remove("activePlayer");
-        this.guest.classList.remove("activePlayer");
-        if (this.activePlayer == "x") {
-            this.host.classList.add("activePlayer");
+        game.host.classList.remove("activePlayer");
+        game.guest.classList.remove("activePlayer");
+        if (game.activePlayer == "x") {
+            game.host.classList.add("activePlayer");
         } else {
-            this.guest.classList.add("activePlayer");
+            game.guest.classList.add("activePlayer");
         }
     }
 }
+
 
 
 window.onload = function(){
     let game = new gameTicTacToe; //Déplacé dans le onLoad car il va chercher des éléments du html donc la page doit être loadée
 
     document.getElementById("pluginResetGrid").addEventListener("click",function(){
-        game.resetBoard();
+        game.resetBoard(game);
     })
 
-    game.setEventToGrid();
+    game.setEventToGrid(game);
 
 }
     
